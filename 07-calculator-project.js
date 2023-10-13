e8eaf8699ccf83027d322ed5fa89743aeb2bb4bf
Zane Palino <101521294+zaneypal@users.noreply.github.com>
2023-10-12 21:52:59 -0400

    Add files via upload

---
+++ 07-calculator-project.js
@@ -0,0 +1,36 @@
+let calculation = '';
+const output = document.getElementById('output');
+
+function updateCalculation(value) {
+
+    if (value === '0' && calculation === '0') {
+    } else if (calculation === '0') {
+        calculation = value;
+    } else {
+        calculation += value;
+    }
+
+    output.innerHTML = calculation;
+}
+
+function clearOutput() {
+    calculation = '0';
+    output.innerHTML = calculation;
+}
+
+function clearAllOutput() {
+    calculation = '0';
+    output.innerHTML = calculation;
+}
+
+function delChar() {
+    if (output.innerHTML === '0') {
+    } else {
+        if (output.innerHTML.length === 1) {
+            calculation = '0';
+        } else {
+            calculation = output.innerText.substring(0, output.innerHTML.length - 1);
+        }
+        output.innerHTML = calculation;
+    }
+}
